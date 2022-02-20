996c91a8e11ec23ace8eb6448c67d7987005cac5
Brian <b.perez3237@gmail.com>
2022-02-20 18:23:37 -0500

    completed Assignemnt

--- main.js
+++ main.js
@@ -5,7 +5,46 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+function heartClick(e) {
+  mimicServerCall()
+    .then(function(res) {
+      
+      if (e.target.className === 'activated-heart') {
+        e.target.textContent = EMPTY_HEART;
+        e.target.className = ''
+      }
+      else {
+        e.target.textContent = FULL_HEART;
+        e.target.className = 'activated-heart'
+      }
+    
+    })
+    .catch(function(res) {
+      console.log(res)
+      const modal = document.getElementById('modal');
+      modal.className = '';
+    
+      const msg = document.getElementById('modal-message');
+      console.log(msg);
+      msg.textContent = res
 
+      setTimeout(() => modal.className = 'hidden',3000);
+    })
+}
+
+
+function setUp() {
+
+  let hearts = document.querySelectorAll('.like');
+
+  hearts.forEach((heart) => {
+    heart.addEventListener('click', (e) => {
+      heartClick(e);
+    });
+  });
+
+}
+setUp();
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
